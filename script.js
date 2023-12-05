ceb4e0609744cbc8034b82c9bebb1c149b3638d4
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-12-06 00:27:02 +0900

    unti

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function resizeWindow(){
     document.getElementById("footer").style.width = window.innerWidth-20 + "px";
+    document.getElementById("navwidth").style.width = window.innerWidth-20 + "px";
 }
 window.addEventListener('resize', resizeWindow);
 resizeWindow();
